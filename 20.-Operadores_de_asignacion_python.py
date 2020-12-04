745ec08050f55f2ce1e030ae208ca9c72119d09c
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 20:37:46 -0600

    Add files via upload

---
+++ 20.-Operadores_de_asignacion_python.py
@@ -0,0 +1,49 @@
+'''
+Este programa muestra el uso de operadores de asignacion.
+'''
+print("Suma en asignación:")
+print("numero1 = 150") # Imprimimos mensaje en pantalla
+numero1 = 150 # Asignamos un valor a la variable numero1
+print("numero1 += 3") # Imprimimos mensaje en pantalla
+numero1 += 3 # Realizamos la suma en asignación, agregamos 3 unidades a la variable numero1
+print(numero1) # Imprimimos el valor de numero1
+print("\n")
+
+print("Resta en asignación: ")
+print("numero2 = 230") # Imprimimos mensaje en pantalla
+numero2 = 230 # Asignamos un valor a numero2
+print("numero2 -= 100") # Imprimimos mensaje en pantalla
+numero2 -=100 # Realizamos la resta en asignación
+print(numero2) # Imprimimos el valor de numero2
+print("\n")
+
+print("Multiplicación en asignación: ")
+print("numero3 = 50") # Imprimimos mensaje en pantalla
+numero3 = 50 # Asignamos un valor a la variable numero3
+print("numero3 *= 3") # Imprimimos mensaje en pantalla
+numero3 *= 3 # Multiplicamos en asignación
+print(numero3) # Imprimimos el valor de numero3
+print("\n")
+
+print("División en asignación: ")
+print("numero4 = 200") # Imprimimos mensaje en pantalla
+numero4 = 200 # Asignamos un valor a numero4
+print("numero4 /= 4") # Imprimimos mensaje en pantalla
+numero4 /= 4 # División en asignación
+print(numero4) # Imprimimos el valor de numero4
+print("\n")
+
+print("Potencia en asignación: ")
+print("numero5 = 3") # Imprimimos mensaje en pantalla
+numero5 = 3 # Asignamos un valor a numero5
+print("numero5 **= 2") # Potencia en asignación
+numero5 **= 2 # Potencia en asignación
+print(numero5)
+print("\n")
+
+print("Módulo en asignación: ")
+print("numero6 = 121")
+numero6 = 121
+print("numero6 %= 2")
+numero6 %= 2
+print(numero6)
